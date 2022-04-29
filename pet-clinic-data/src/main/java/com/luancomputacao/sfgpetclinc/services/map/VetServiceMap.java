54acb93542c712560b1ece9338510fddb87ad8f0
LuanComputacao <eng.luanroger@gmail.com>
2022-04-29 01:09:19 -0300

    feat: Implement Map Based Services Closes #6

---
+++ pet-clinic-data/src/main/java/com/luancomputacao/sfgpetclinc/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.luancomputacao.sfgpetclinc.services.map;
+
+import com.luancomputacao.sfgpetclinc.model.Vet;
+import com.luancomputacao.sfgpetclinc.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
